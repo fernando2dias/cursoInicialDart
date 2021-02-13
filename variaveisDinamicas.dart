2042830d02126c8ad7852066bad4e2649fba7b6a
fernando2dias <fernando3dias@gmail.com>
2021-02-13 12:15:50 -0300

    variaveis dinamicas

---
+++ variaveisDinamicas.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  var idade = 35;
+  var pi = 3.1415;
+  var texto = "Eu tenho $idade anos e o valor de pi é $pi";
+
+  dynamic numero = 40;
+  print(numero);
+
+  numero = 30.3;
+  print(numero);
+
+  numero = "teste";
+  print(numero);
+
+  print(texto);
+}
