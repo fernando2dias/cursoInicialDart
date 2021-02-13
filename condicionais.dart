18fe67484fbafaf730be52e966741ce76b7b10d8
Fernando Dias Motta <fernando.motta@fit-tecnologia.org.br>
2021-02-13 12:45:10 -0300

    condicionais

---
+++ condicionais.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  print("testando condicionais");
+  int idade = 25;
+
+  if (idade >= 18) {
+    print("Você tem mais de 18 anos pode entrar!");
+  } else {
+    print("Você não tem permissão de entrada.. vai chorar para seus pais =(");
+  }
+}
