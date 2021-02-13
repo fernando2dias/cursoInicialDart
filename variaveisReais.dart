5f7676b7b15c2ed7d4d5cc7c9be6cd4af85c431a
fernando2dias <fernando3dias@gmail.com>
2021-02-13 11:31:29 -0300

    numeros reais

---
+++ variaveisReais.dart
@@ -0,0 +1,6 @@
+void main() {
+  double salario = 1250.50;
+  double novo_salario = salario * 2;
+  print("Meu salário é de R\$ $salario");
+  print("meu novo salario vai ser R\$ $novo_salario");
+}
